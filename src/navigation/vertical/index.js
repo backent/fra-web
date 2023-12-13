a533c240d6e004bd26906c011727019d128d71d1
malik <assassinate75@gmail.com>
2023-12-13 23:37:22 +0700

    add icon to sidebar nav

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -2,17 +2,21 @@ export default [
   {
     title: 'Fra Document',
     to: { name: 'fra-document' },
+    icon: { icon: 'tabler-file-description' }
   },
   {
     title: 'Tracking Process FRA',
     to: { name: 'tracking-process-fra' },
+    icon: { icon: 'tabler-checklist' }
   },
   {
     title: 'Monitoring & Evaluation',
     to: { name: 'monitoring-evaluation' },
+    icon: { icon: 'tabler-device-imac-check' }
   },
   {
     title: 'Account Request',
     to: { name: 'account-request' },
+    icon: { icon: 'tabler-user-check' }
   },
 ]
